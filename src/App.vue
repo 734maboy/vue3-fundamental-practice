b26f1993ba37923f61645ab6ae811a39ac9efff3
734maboy <supamem74@gmail.com>
2022-08-09 22:09:56 +0300

    feature-posts-list-and-post-form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    LIKES: <strong>{{likes}}</strong>
-    <button
-        @click="addLike"
-    >LIKE</button>
+  <div class="app">
+    <form>
+      <input
+          v-model="postTitle"
+          class="input"
+          type="text"
+          placeholder="Post title"
+      >
+      <input
+          v-model="postBody"
+          class="input"
+          type="text"
+          placeholder="Post description"
+      >
+      <button class="btn" @click="createPost">Create post</button>
+    </form>
+    <div
+        v-for="post in posts"
+        :key="`post_ux_${post.id}`"
+        class="post">
+      <div> <strong>Post title:</strong> {{post.id}}. {{post.title}} </div>
+      <div> <strong>Post description:</strong> {{post.body}} </div>
+    </div>
   </div>
 </template>
 
@@ -12,18 +30,67 @@ export default {
   name: 'App',
 
   data: () => ({
-    likes: 0,
+    posts: [
+      { id: 1, title: 'JS', body: 'JS Desc' },
+      { id: 2, title: 'C#', body: 'C# Desc' },
+      { id: 3, title: 'React', body: 'React Desc' },
+      { id: 4, title: 'Vue', body: 'Vue Desc' },
+      { id: 5, title: 'ASP.NET', body: 'ASP.NET Desc' },
+    ],
+
+    postTitle: '',
+    postBody: '',
   }),
 
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
+    createPost(e) {
+      e.preventDefault();
+      this.posts.push({
+        id: this.posts.length + 1,
+        title: this.postTitle,
+        body: this.postBody,
+      })
+    }
   },
 
 }
 </script>
 
-<style scoped>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  .post {
+    padding: 15px;
+    border: 3px solid #e34de3;
+    margin: 10px 0;
+  }
+
+  .input {
+    width: 100%;
+    border: 1px solid #e34de3;
+    padding: 10px;
+    margin-top: 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 5px 10px;
+    background: #8a028a;
+    color: white;
+    font-weight: 400;
+  }
+
+  .app {
+    padding: 5px 20px;
+  }
 </style>
