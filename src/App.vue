2b3345cb5aef94351a11ef4951eec628328a2e91
734maboy <supamem74@gmail.com>
2022-08-09 21:46:36 +0300

    just-fun

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    LIKES: <strong>{{likes}}</strong>
+    <button
+        @click="addLike"
+    >LIKE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    likes: 0,
+  }),
+
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
